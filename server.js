04420cac2d60d5b91f835a6492988f5e551f504e
daveyrojo <d.vincenteldridge@gmail.com>
2021-05-10 17:51:42 -0400

    brought in dependencies, server launching

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 const path = require("path");
-
 const express = require("express");
 const routes = require("./routes");
-
 const session = require("express-session");
-
+const MongoStore = require("connect-mongo");
+const mongoose = require("mongoose");
+const passport = require("passport");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,3 +14,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
+
+app.use(
+  session({
+    secret: "super secret",
+    store: MongoStore.create({ mongoUrl: "mongodb://localhost/uwwDb" }),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/uwwDb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});

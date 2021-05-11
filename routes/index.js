c6e09365c60fe55f343d3757f0762ab17b5810b7
daveyrojo <d.vincenteldridge@gmail.com>
2021-05-10 20:34:50 -0400

    began building api routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
+const path = require("path");
 const router = require("express").Router();
+const apiRoutes = require("./apiRoutes");
 
+// API Routes
+router.use("/api", apiRoutes);
+
+// If no API routes hit - go to 404.html
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/404.html"));
+});
 module.exports = router;
